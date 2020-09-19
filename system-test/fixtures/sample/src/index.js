2a098e763d2dd8abc030a0bbbaa738d2ace8fde0
yoshi-automation <yoshi-automation@google.com>
2020-09-19 02:28:14 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -17,7 +17,7 @@
 // ** All changes to this file may be overwritten. **
 
 /* eslint-disable node/no-missing-require, no-unused-vars */
-const tables = require('@google/area120-tables');
+const tables = require('tables');
 
 function main() {
   const tablesServiceClient = new tables.TablesServiceClient();
